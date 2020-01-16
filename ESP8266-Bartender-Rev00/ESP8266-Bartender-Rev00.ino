bd2556607d35291e3604f8ea458d2c86c8e34b25
axiodn <edwardnichols@live.com>
2020-01-15 22:16:50 -0500

    init MQTT functionality for ESP8266

---
+++ ESP8266-Bartender-Rev00/ESP8266-Bartender-Rev00.ino
@@ -0,0 +1,48 @@
+/*
+   AUTHOR                  : Edward Daniel Nichols
+   LAST CONTRIBUTION DATE  : January 15, 2020
+
+   Rev00 - Objective:
+   - Print any data to MQTT server
+   - Print data from MQTT server
+*/
+
+/*Required local libraries: IN THIS ORDER */
+#include    "WifiFunctions.h"
+#include    "MqttFunctions.h"
+
+/*Magnetometer global setup: header library files. */
+#include   <Wire.h>
+
+String  article;
+void setup()
+{
+  // Start up the serial port connection and announce title.
+  Serial.begin( 115200 );
+  Serial.println( "ESP8266 MQTT Bartender - Rev00" );
+
+  WifiSetup();                                // Setup and connect to WiFi.
+
+  // Prompt MQTT to connect for the first time.
+  MQTT_connect( 250 );
+}
+
+void loop()
+{
+  // retrieve sensor information here
+
+  // Prepare string to publish the values to MQTT
+  article = String("bartender: I'm alive!");
+  
+  // Verify MQTT broker is connected.
+  if ( !mqtt.connected() )
+    MQTT_connect(500);
+
+  // Convert the String object to a character array, so that MQTT can actually publish it. */
+  char post[64];
+  article.toCharArray(post, 128);
+  devOutput.publish(post);
+
+  // Delay to give MQTT time to catch up.
+  delay(500);
+};
---
+++ ESP8266-Bartender-Rev00/MqttFunctions.h
@@ -0,0 +1,46 @@
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define MQTT_SERVER     "192.168.1.78"          // URL to the RPi running MQTT
+#define MQTT_SERVERPORT 1883                    // MQTT service port
+#define MQTT_USERNAME   ""                      // In preparation for securing the MQTT broker.
+#define MQTT_PASSWORD   ""
+#define MQTT_ID         "bartender"         // Identifier to MQTT broker
+#define SENSOR01_PUB	  "testTopic"  // Not accessible within Chatterbox, but through Pi mosquitto-clients
+
+String                  ID        = "ID - ";
+String                  mqttID( MQTT_ID );
+
+////  Requires: '#include "WifiFunctions.h"' in main program
+////  before:   '#include "MqttFunctions.h"'.
+Adafruit_MQTT_Client    mqtt( &wifiClient, MQTT_SERVER, MQTT_SERVERPORT );
+Adafruit_MQTT_Publish   devOutput = Adafruit_MQTT_Publish( &mqtt, SENSOR01_PUB );
+
+void MQTT_connect( int blockingTime )         // Function to connect and reconnect as necessary;
+{ 
+  int8_t rc;
+  if ( mqtt.connected() )                     // If already connected...
+  {
+    mqtt.processPackets( blockingTime );
+    if ( !mqtt.ping() )
+      mqtt.disconnect();
+  }
+  else
+  {
+    Serial.println( "Connecting to MQTT..." );
+
+    uint8_t retries = 15;
+    while ( (rc = mqtt.connect()) != 0 )      // connect will return 0 for connected
+    {
+      Serial.println( mqtt.connectErrorString( rc ) );
+      Serial.println( "Retrying MQTT connection in five seconds..." );
+      mqtt.disconnect();
+      delay( 5000 );                          // wait 5 seconds
+      retries--;
+      if ( retries == 0 ) 
+        while ( true );     // Basically die and wait for WDT to reset.
+    }
+    Serial.println( "MQTT Connected." );
+    Serial.print( "MQTT ID: " ); Serial.println( mqttID );
+  }
+}
